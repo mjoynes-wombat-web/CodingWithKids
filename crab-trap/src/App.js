6c0e7682c10cb16486e7b79e80f3493ae427b893
ssmith-wombatweb <ssmith@wombatweb.us>
2018-07-23 21:17:00 -0700

    Playing with CSSTransitions.

--- crab-trap/src/App.js
+++ crab-trap/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import 'normalize.css';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import StartGame from './scenes/StartGame';
 import Crab from './components/Crab';
@@ -72,27 +73,39 @@ class App extends Component {
           screenWidth={screenWidth}
           display
         />
-        {rotate ? <PleaseRotate /> : null}
-        {!fullscreen && !rotate
-          ? (
-            <StartGame
-              startGame={this.startGame}
-              screenWidth={screenWidth}
-            />
-          )
-          : null}
-        {!rotate && fullscreen
-          ? (
-            <GameBoard
-              hidingSpots={hidingSpots}
-              hidingSpotWidth={hidingSpotWidth}
-              enterFullscreen={this.enterFullscreen}
-              fullscreen={fullscreen}
-              screenWidth={screenWidth}
-            />
-          )
-          : null
-        }
+        <TransitionGroup className="scenes">
+          {rotate ? (
+            <CSSTransition classNames="scene" timeout={{ enter: 2000, exit: 2000 }}>
+              <PleaseRotate />
+            </CSSTransition>
+          ) : null}
+          {!rotate && fullscreen
+            ? (
+              <CSSTransition component="div" classNames="scene game-board" timeout={{ enter: 2000, exit: 2000 }}>
+                <div>
+                  <GameBoard
+                    hidingSpots={hidingSpots}
+                    hidingSpotWidth={hidingSpotWidth}
+                    enterFullscreen={this.enterFullscreen}
+                    fullscreen={fullscreen}
+                    screenWidth={screenWidth}
+                  />
+                </div>
+              </CSSTransition>
+            )
+            : null
+          }
+          {!fullscreen && !rotate
+            ? (
+              <CSSTransition classNames="scene" timeout={{ enter: 2000, exit: 2000 }}>
+                <StartGame
+                  startGame={this.startGame}
+                  screenWidth={screenWidth}
+                />
+              </CSSTransition>
+            )
+            : null}
+        </TransitionGroup>
         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" />
       </main>
     );
@@ -116,111 +129,136 @@ const colors = {
 };
 
 export default styled(App)`
-    margin: 0;
-    padding: 0;
-    background: url(${sand});
+margin: 0;
+padding: 0;
+background: url(${sand});
+display: flex;
+flex-direction: column;
+width: 100vw;
+height: 100vh;
+
+h1 {
+  color: ${colors.darkOrange};
+  text-shadow: 0.125rem 0.125rem 0 ${colors.darkBrown};
+  font-size: 4rem;
+  font-family: 'Lobster', Arnoldboecklin, cursive;
+}
+
+> * {
+  flex: 0 1;
+}
+
+#startGame {
+  background: url(${sand});
+  position: relative;
+  z-index: 1000;
+  min-height: 100vh;
+}
+
+.scenes {
+  position: relative;
+  .game-board-enter * {
+    opacity: 0;
+    transition: opacity 0.5s linear 1.5s;
+    &.game-board-enter-active * {
+      opacity: 1;
+    }
+  }
+
+  .game-board-exit * {
+    opacity: 1;
+    transition: opacity 0.5s linear 1.5s;
+    &.game-board-exit-active * {
+      opacity: 0;
+    }
+  }
+  .scene-enter {
+    perspective: 1000;
+    transition: all 0.5s linear 1.5s;
+    transform: rotateX(180deg);
+    opacity: 0;
+    &.scene-enter-active {
+      transform: rotateX(0deg);
+      opacity: 1;
+    }
+  }
+  .scene-exit {
+    perspective: 1000;
+    transition: all 0.5s linear 1.5s;
+    opacity: 1;
+    transform: rotateX(0deg);
+    &.scene-exit-active {
+      opacity: 0;
+      transform: rotateX(180deg);
+    }
+  }
+}
+
+.grid {
+  position: absolute;
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  z-index: 1000;
+  pointer-events: none;
+
+  .column {
+    flex: 1;
     display: flex;
     flex-direction: column;
-    width: 100vw;
-    height: 100vh;
 
-    h1 {
-      color: ${colors.darkOrange};
-      text-shadow: 0.125rem 0.125rem 0 ${colors.darkBrown};
-      font-size: 4rem;
-      font-family: 'Lobster', Arnoldboecklin, cursive;
-    }
-    
-    > * {
-      flex: 0 1;
-    }
-
-    .grid {
-      position: absolute;
-      min-width: 100vw;
-      min-height: 100vh;
-      display: flex;
-      z-index: 1000;
-      pointer-events: none;
-
-      .column {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-
-        .row {
-          flex: 1;
-          padding: 1rem;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-
-          .hiding-spot {
-            width: 12vw;
-            height: 100%;
-            background-color: red;
-            opacity: 0.25;
-            pointer-events: all;
-          }
-        }
-      }
-    }
-  
-    .buttons {
-      position: absolute;
-      z-index: 10000;
-      top: 0;
-      left: 0;
-    }
-    
-    button {
-      appearance: none;
-      border: none;
-      border-radius: 0.25rem;
-      padding: 0.5rem;
-      background-color: ${colors.orange};
-      color: white;
-      outline: none;
-      margin: 0.25rem;
-      cursor: pointer;
-      transition: background-color 0.25s;
-
-      &:hover {
-        background-color: ${colors.darkOrange};
-      }
-
-      &:active, &:active:hover {
-        background-color: ${colors.gold};
-      }
-
-      &:disabled {
-        background-color: #999;
-      }
-
-      svg.fa-expand, svg.fa-compress, svg.fa-walking {
-        width: 1rem;
-        height: 1rem;
-      }
-    }
-    .state {
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
+    .row {
       flex: 1;
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      * { margin: 0.125rem; }
-      h2 { font-size: 0.75rem; }
-      .details {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-        text-align: left;
-        p {
-          font-size: 0.625rem;
-          flex: 1;
-        }
+      padding: 1rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .hiding-spot {
+        width: 12vw;
+        height: 100%;
+        background-color: red;
+        opacity: 0.25;
+        pointer-events: all;
       }
     }
+  }
+}
+
+.buttons {
+  position: absolute;
+  z-index: 10000;
+  top: 0;
+  left: 0;
+}
+
+button {
+  appearance: none;
+  border: none;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  background-color: ${colors.orange};
+  color: white;
+  outline: none;
+  margin: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.25s;
+
+  &:hover {
+    background-color: ${colors.darkOrange};
+  }
+
+  &:active, &:active:hover {
+    background-color: ${colors.gold};
+  }
+
+  &:disabled {
+    background-color: #999;
+  }
+
+  svg.fa-expand, svg.fa-compress, svg.fa-walking {
+    width: 1rem;
+    height: 1rem;
+  }
+}
 `;
